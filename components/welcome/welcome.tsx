78ff33efd05e926b1491f9b6107018bd899dd79c
cyburns <72774499+cyburns@users.noreply.github.com>
2024-06-01 11:49:05 -0400

    log in sign up done

--- components/welcome/welcome.tsx
+++ components/welcome/welcome.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Contact from "../home/contact";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="flex justify-center items-center flex-col mt-56">
+      <Contact />
+      <div>
+        <h1 className="text-green-500 dark:text-green-400 mt-24">
+          Thank you for joing the BRIGHT community. We will have content up and
+          runningn soong!
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
